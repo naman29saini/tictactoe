31bc7c04e32c56a712474e1fc471415689b2bd73
Naman <109201101+naman29saini@users.noreply.github.com>
2024-11-04 14:29:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './Components/Footer/Footer';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        Copyright &copy; 2024, Naman Saini - All Rights Reserved
+    </footer>
+  )
+}
+
+export default Footer
